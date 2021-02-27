063dd7c5241dc73765b9b779f13356b30ba22f62
Matt Dove <mattdove22@gmail.com>
2021-02-26 21:28:49 -0800

    Shaping question one, laying the groundwork

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a Grey's Anatomy personality quiz!
+Currently, you can only get one of the five interns!
---
+++ script.js
@@ -0,0 +1,23 @@
+function questionOne {
+let yang1 = 0;
+let mer1 = 0;
+let alex1 = 0;
+let izzie1 = 0;
+let george1 = 0;
+console.log("How fare are you willing to go to get into the operating room?");
+console.log("a) I would lie, cheat, or steal to get a good surgery.");
+console.log("b) I would plead with these attendings to let me in.");
+console.log("c) I would hope for the best, whatever happens happens.");
+console.log("d) I would prefer not to be in the operating room.");
+choiceOne = prompt("a/b/c/d?: ").toLowerCase();
+if (choiceOne = "a") {
+    let yang1 += 2; 
+    let mer1 += 1;
+    let alex1 += 0;
+    let george1 -= 2;
+    let izzie1 -= -1;
+  } else if (ChoiceOne = "b") {
+    let yang1 += 1;
+    let 
+  }
+}
